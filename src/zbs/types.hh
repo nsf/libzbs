d2c9309bffdbb309b504ed4ba2641874aac24756
nsf <no.smile.face@gmail.com>
2013-01-27 19:33:36 +0600

    Add types.hh with basic precise type definitions.

---
+++ src/zbs/types.hh
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+
+namespace zbs {
+
+typedef uint8_t  uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+typedef int8_t   int8;
+typedef int16_t  int16;
+typedef int32_t  int32;
+typedef int64_t  int64;
+typedef uint8    byte;
+typedef int32    rune;
+
+} // namespace zbs
