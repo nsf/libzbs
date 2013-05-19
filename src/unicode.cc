971fedd08687d83ee95dcda6da104482282f6ab0
nsf <no.smile.face@gmail.com>
2013-05-19 16:24:50 +0600

    Add copyright notice to the unicode.cc file.

--- src/unicode.cc
+++ src/unicode.cc
@@ -1,3 +1,7 @@
+// This file may contain portions of the code derived/adopted from the Go
+// standard library, which is covered by a BSD-style license. You can find more
+// details in 3rdparty/go_license.txt file.
+
 #include "zbs/unicode.hh"
 #include "zbs/slice.hh"
 
